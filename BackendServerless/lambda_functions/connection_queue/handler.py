73c22c28f0eb6980e1f219d834dc6b7c32db08dd
Francisco Scholborgh Quintero <frankymster@gmail.com>
2021-03-27 12:51:49 -0500

    Python script aws methods created to reuse common methods implemented in lambda functions (calls to APIGateway, SQS, and invoke other lambda functions)

--- BackendServerless/lambda_functions/connection_queue/handler.py
+++ BackendServerless/lambda_functions/connection_queue/handler.py
@@ -1,28 +1,13 @@
 import json
-import boto3
-import logging
-from botocore.exceptions import ClientError
+from utils.aws_methods import get_response, send_update_wsapi
 from utils.database import TicTacToeDB
 
-
 apiUrl = "https://psmhp2yn01.execute-api.us-east-1.amazonaws.com/dev"
-client = boto3.client("apigatewaymanagementapi", endpoint_url=apiUrl)
 db = TicTacToeDB("mongodb+srv://tictactoedb:fa6PQ1Xy8CCRoUou@cluster0.hb7sg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", "tictactoe_db")
 
-def _get_response(statusCode: int, message: object):
-    return {
-        "statusCode": statusCode,
-        "body": json.dumps(message)
-    }
-    
-def send_update(users:list, message:object):
-    for user in users:
-        try:
-            id= user.get("_id") 
-            client.post_to_connection(ConnectionId=id, Data=json.dumps(message))
-        except:
-            pass
-        
+#:param userID: String user identification in the database collections of users
+#:param username: String name of a users
+#:return: returns dir with the code state of the response (int) and a message with game status and players and spectator access         
 def connect_event(userID:str, username:str):
     response = db.register_player(userID, username)
     users = db.get_allUsers()
@@ -31,7 +16,7 @@ def connect_event(userID:str, username:str):
         players = [game_status.get("player1")]
         if response["ready_toplay"]:
             players.append(game_status.get("player2"))
-            send_update(players, {"action":"START_GAME", "response_data": response})
+            send_update_wsapi(apiUrl, players, {"action":"START_GAME", "response_data": response})
             player1_id = game_status.get("player1").get("_id")
             player2_id = game_status.get("player2").get("_id")
             spectators = []
@@ -40,15 +25,15 @@ def connect_event(userID:str, username:str):
                 if user_id != player1_id and user_id != player2_id:
                     spectators.append(user)
             response[""] = True
-            send_update(spectators, {"action":"SPECTATOR_JOIN", "response_data": response})
+            send_update_wsapi(apiUrl, spectators, {"action":"SPECTATOR_JOIN", "response_data": response})
         else:
-            send_update(players, {"action":"WAITING_RIVAL", "response_data": response})
+            send_update_wsapi(apiUrl, players, {"action":"WAITING_RIVAL", "response_data": response})
     except KeyError:
         response["spectator"] = username
-        send_update(users, {"action":"SPECTATOR_JOIN", "response_data": response})
-    return _get_response(200, "connected")
+        send_update_wsapi(apiUrl, users, {"action":"SPECTATOR_JOIN", "response_data": response})
+    return get_response(200, "connected")
 
-def lambda_handler(event, context):
+def connection_queue(event, context):
     records = event.get("Records")[0]
     body = json.loads(records.get("body")) 
     userID = body.get("playerID")
--- BackendServerless/lambda_functions/tictactoe_gameUpdates/handler.py
+++ BackendServerless/lambda_functions/tictactoe_gameUpdates/handler.py
@@ -1,31 +1,22 @@
 import json
-import boto3
+from utils.aws_methods import get_response, send_update_wsapi
 from utils.database import TicTacToeDB
 
 apiUrl = "https://psmhp2yn01.execute-api.us-east-1.amazonaws.com/dev"
-client = boto3.client("apigatewaymanagementapi", endpoint_url=apiUrl)
 db = TicTacToeDB("mongodb+srv://tictactoedb:fa6PQ1Xy8CCRoUou@cluster0.hb7sg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", "tictactoe_db")
 
-def _get_response(statusCode: int, message: object):
-    return {
-        "statusCode": statusCode,
-        "body": json.dumps(message)
-    }
-
-def send_update(users:list, message:object):
-    for user in users:
-        try:
-            id= user.get("_id") 
-            client.post_to_connection(ConnectionId=id, Data=json.dumps(message))
-        except:
-            pass
-
+#:param line: list with a row of the tic tac toe board to evaluate if it's a winning one
+#:return: returns True if the evaluated row has the same token (O or X), otherwise return False
 def checkWinningLine (line:list):
     return line[0] != '' and (line[0] == line[1]) and (line[1] == line[2])
 
+#:param line: list with a row of the tic tac toe board to evaluate if it's filled
+#:return: returns True if the evaluated row it's filled, otherwise return False
 def checkFilledRow (row:list):
     return row[0] != '' and row[1] != '' and row[2] != ''
 
+#:param line: list of list (3x3) representing the columns and rows of the tic tac toe game board
+#:return: returns True if there is a winning  row, a line of three O or X, otherwise return False
 def checkWinner(board: list):
     check_values = [ board[0], board[1], board[2], [board[0][0], board[1][0], board[2][0]], [board[0][1], board[1][1], board[2][1]],
         [board[0][2], board[1][2], board[2][2]], [board[0][0], board[1][1], board[2][2]], [board[0][2], board[1][1], board[2][0]] ]
@@ -35,6 +26,8 @@ def checkWinner(board: list):
             return True
     return False
 
+#:param line: list of list (3x3) representing the columns and rows of the tic tac toe game board
+#:return: returns True if the game is tie, otherwise return False
 def checkDraw(board: list):
     return checkFilledRow(board[0]) and checkFilledRow(board[1]) and checkFilledRow(board[2])
 
@@ -55,7 +48,7 @@ def game_update(event, context):
         else:
             action = "CHANGE_TURN" 
         users = db.get_allUsers()
-        send_update(users, {"action":action, "response_data":game_status})
-        return _get_response(200, "updated")
+        send_update_wsapi(apiUrl, users, {"action":action, "response_data":game_status})
+        return get_response(200, "updated")
     else:
-         return _get_response(400, "It's not your turn")
+         return get_response(400, "It's not your turn")
